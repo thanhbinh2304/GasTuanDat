c1ad8ebed7205c1ff4c185bc5d58603e0ae6571f
thanhbinh2304 <binh2304tto@gmail.com>
2026-06-21 11:09:06 +0700

    feat: configure Spring Security with JWT authentication, stateless session management, and CORS settings

--- src/main/java/com/example/GasTuanDat/common/config/SecurityConfig.java
+++ src/main/java/com/example/GasTuanDat/common/config/SecurityConfig.java
@@ -105,7 +105,7 @@ public class SecurityConfig {
         CorsConfiguration config = new CorsConfiguration();
 
         config.setAllowedOrigins(
-                List.of("http://localhost:3000", "https://nppgastuandat.vercel.app"));
+                List.of("http://localhost:3000", "https://nppgastuandat.io.vn", "https://nppgastuandat.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "X-Requested-With"));
         config.setAllowCredentials(true);
